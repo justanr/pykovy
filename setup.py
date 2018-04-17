43e2a4691091e6f07eebc45e3f0d5953b52b7f6d
Alec Nikolas Reiter <alecreiter@gmail.com>
2018-04-17 00:24:41 -0400

    travis setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 if __name__ == "__main__":
     setup(
         name="pykovy",
-        version="1.0",
+        version="1.1",
         author="Alec Nikolas Reiter",
         author_email="alecreiter@gmail.com",
         description="Real simple markov chains",
